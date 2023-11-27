3a701a818c6b1767929f3ddbacdcd2f4da9cbb30
ErnestoFolting <danilsmy.edu@gmail.com>
2023-11-27 22:44:26 +0200

    Add minor fixes after merge

--- IPZ_docker/IPZ_docker/Program.cs
+++ IPZ_docker/IPZ_docker/Program.cs
@@ -4,29 +4,9 @@ using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 string connectionString = builder.Configuration.GetConnectionString("MongoDBConnection");
 
 var mongoClient = new MongoClient(connectionString);
-
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), options =>
-    options.EnableRetryOnFailure());
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    var databases = cursor.ToList();
-    foreach (var database in databases)
-    {
-        Console.WriteLine(database);
-    }
-}
 
 builder.Services.AddSingleton<IMongoClient>(mongoClient);
 builder.Services.AddSingleton<IMongoDatabase>(provider =>
@@ -35,9 +15,14 @@ builder.Services.AddSingleton<IMongoDatabase>(provider =>
     return client.GetDatabase("Sellers");
 });
 
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), options =>
+    options.EnableRetryOnFailure());
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
